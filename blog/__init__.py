fdf5d0519c20668b6f0aa93eb92fdf5728e4f876
tarn <tarn.barford@retresco.de>
2013-02-06 19:39:03 +0100

    post list + posts

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+from app.data import db_session, Post
 
 blog = Blueprint('blog', __name__,
                  template_folder='templates')
@@ -6,4 +7,12 @@ blog = Blueprint('blog', __name__,
 
 @blog.route('/journal')
 def index():
-    return render_template('blog/index.html')
+    posts = Post.query.all()
+    return render_template('blog/index.html', posts=posts)
+
+@blog.route('/journal/<slug>')
+def post(slug):
+    post = Post.query.filter_by(slug=slug).first()
+    if not post:
+        abort(404)
+    return render_template('blog/post.html', post=post)
